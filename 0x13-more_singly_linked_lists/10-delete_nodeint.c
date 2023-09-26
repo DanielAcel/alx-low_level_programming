496a575a7e2f6d308dc34d985735fa7038f145d9
Daniel <lawrencencubedaniel@gmail.com>
2023-09-26 12:21:22 -0700

    DANNT

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,4 +25,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (i < index - 1)
 	{
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
 
+
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+
+	return (1);
+}
